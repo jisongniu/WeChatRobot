0b1eb155693500ade93e43ca5331d56361691bcb
jisongniu <529058747@qq.com>
2024-12-19 20:20:09 +0800

    时间管理测试bug修复

--- main.py
+++ main.py
@@ -11,26 +11,10 @@ from robot import Robot, __version__
 from wcferry import Wcf
 from job_mgmt import JobManager
 
-
-def weather_report(robot: Robot) -> None:
-    """模拟发送天气预报
-    """
-
-    # 获取接收人
-    receivers = ["filehelper"]
-
-    # 获取天气，需要自己实现，可以参考 https://gitee.com/lch0821/WeatherScrapy 获取天气。
-    report = "这就是获取到的天气情况了"
-
-    for r in receivers:
-        robot.sendTextMsg(report, r)
-        # robot.sendTextMsg(report, r, "notify@all")   # 发送消息并@所有人
-
-
-class Main:
-    def __init__(self):
-        config = Config()
-        wcf = Wcf(debug=True)
+def main(chat_type: int):
+    """主函数"""
+    config = Config()
+    wcf = Wcf(debug=True)
 
     def handler(sig, frame):
         wcf.cleanup()  # 退出前清理环境
@@ -48,15 +32,11 @@ class Main:
     # robot.enableRecvMsg()     # 可能会丢消息？
     robot.enableReceivingMsg()  # 加队列
 
-    # 在Robot类的__init__中添加:
-    self.job_mgr = JobManager(wcf)
-
     # 让机器人一直跑
     robot.keepRunningAndBlockProcess()
-
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('-c', type=int, default=0, help=f'选择模型参数序号: {ChatType.help_hint()}')
-    args = parser.parse_args().c
-    main(args)
+    args = parser.parse_args()
+    main(args.c)
